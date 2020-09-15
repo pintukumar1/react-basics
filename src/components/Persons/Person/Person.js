c6a859722f3e23cf2502d1081f9cd98cc75039a4
pintukumar1 <kumarpintu1713122@gmail.com>
2020-09-15 09:37:47 +0530

    first commit

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Aux from '../../../hoc/Auxiliary';
+import withClass from '../../../hoc/withClass';
+import classes from './Person.css';
+import AuthContext from '../../../context/auth-context';
+
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+        this.inputElementRef= React.createRef();
+    }
+
+    static contextType = AuthContext;
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+        console.log(this.context.authenticated)
+    }
+
+    render() {
+        console.log('[Person.js] rendering...');
+        return (
+            <Aux>
+                <AuthContext.Consumer>
+                    {  context =>
+                        context.authenticated? <p> Authenticated! </p> : <p> Please Log in </p> }
+                </AuthContext.Consumer>
+                <p onClick={this.props.click}>
+                    I am {this.props.name} and I am {this.props.age} years old!!</p>
+                <p key="i2">{this.props.children}</p>
+                <input
+                    key="i3"
+                    //ref={(inputEl)=>{this.inputElement=inputEl}}
+                    ref={this.inputElementRef}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name}
+                />
+            </Aux>
+        );
+    }
+}
+
+ Person.propTypes = {
+    click:PropTypes.func,
+    name:PropTypes.string,
+    age:PropTypes.number,
+    changed:PropTypes.func
+ };
+
+export default withClass(Person,classes.Person);
